7b51d932705e01292d06a6a683b18dcc9a5474eb
jhonenV <jhonendvalant@gmail.com>
2022-04-01 22:55:43 -0500

    collision detection

--- player.py
+++ player.py
@@ -7,8 +7,12 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32, 64))
         self.image.fill('blue')
         self.rect = self.image.get_rect(topleft=pos)
+
+        # Movement
         self.direction = pygame.math.Vector2(0, 0)
         self.speed = 5
+        self.gravity = 0.8
+        self.jump_speed = -16
 
     def get_input(self):
         keys = pygame.key.get_pressed()
@@ -20,6 +24,15 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction.x = 0
 
+        if keys[pygame.K_SPACE] or keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.jump()
+
+    def apply_gravity(self):
+        self.direction.y += self.gravity
+        self.rect.y += self.direction.y
+
+    def jump(self):
+        self.direction.y = self.jump_speed
+
     def update(self):
         self.get_input()
-        self.rect.x += self.direction.x * self.speed
